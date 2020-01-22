1368980316b8673f42f9acb2f6c34806c0482c24
Josh Blumberg <jlblumberg@gmail.com>
2020-01-22 15:33:16 +0000

    challenge 10 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
-require "docking_station"
+require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to(:release_bike) }
+  it 'new object should respond to release_bike' do
+     expect(subject).to respond_to(:release_bike)
+  end
+  it 'should get a working bike' do
+    expect(subject.release_bike).to be_a Bike
+    expect(subject.release_bike.working?).to be true
+  end
 end
