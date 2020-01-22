d40fdc0e11a6cc82b9361a3152d6144583e00e3f
Jason <jasylwong@gmail.com>
2020-01-22 16:44:18 +0000

    Chapter 11 started

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,27 @@
 require_relative 'bike'
 
 class DockingStation
-  def release_bike
-    Bike.new
+  attr_reader :bike, :biked_is_docked
+
+  def initialize
+    @bike = Bike.new
+    @bike_is_docked = true
+    # puts @bike_is_docked
   end
+
+  def release_bike
+    @bike_is_docked = false
+    @bike
+  end
+
+  def dock(bike)
+    @bike_is_docked = true
+  end
+
+  def bike_is_docked
+    return @bike_is_docked
+  end
+
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,23 @@ describe DockingStation do
   it 'new object should respond to release_bike' do
      expect(subject).to respond_to(:release_bike)
   end
+  
   it 'should get a working bike' do
     expect(subject.release_bike).to be_a Bike
     expect(subject.release_bike.working?).to be true
+  end
+
+  it 'allows bikes to be docked' do
+    expect(subject).to respond_to(:dock)
+  end
+
+  it 'returns true when docking' do
+    expect(subject.dock(@bike)).to be true
+  end
+
+  it 'returns true when a bike is docked' do
+    station = DockingStation.new
+    station.release_bike
+    expect(station.bike_is_docked).to be false
   end
 end
