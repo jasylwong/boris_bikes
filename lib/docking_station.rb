6c7a87277f41f29bfe134ce963e5f07b38119172
Jason <jasylwong@gmail.com>
2020-01-24 17:19:49 +0000

    Challenge 15 added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,21 @@ class DockingStation
     end
 
     def release_bike
-      raise 'No bikes available' unless @bikes.length != 0
+      raise 'No bikes available' if empty?
       @bikes.shift 
     end
 
     def dock(bike)
-      raise "Station already full" if @bikes.length >= 20
+      raise "Station already full" if full?
       @bikes << bike
+    end
+    
+    private
+    def full?
+      @bikes.length >= 20
+    end
+
+    def empty?
+      @bikes.empty?
     end
 end
