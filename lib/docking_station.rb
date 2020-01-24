f5b0ee742a840789ef921d232844a52249f69769
Ziad <ziad1991@hotmail.co.uk>
2020-01-24 16:59:23 +0000

    challenge 14 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,19 @@
 require_relative 'Bike'
 
 class DockingStation
-    attr_reader :bike
+    attr_reader :bikes
+
+    def initialize
+      @bikes = []
+    end
 
     def release_bike
-      raise 'No bikes available' unless @bike
-      @bike
+      raise 'No bikes available' unless @bikes.length != 0
+      @bikes.shift
     end
 
     def dock(bike)
-      # @bike = 0
-      raise "Station already full" if @bike
-      @bike = bike
+      raise "Station already full" if @bikes.length >= 20
+      @bikes << bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,7 +10,7 @@ describe DockingStation do
     it 'releases a bike when a bike is available' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      expect(subject.release_bike).to be_an_instance_of(Bike)
     end
 
     it 'raises an error when no bikes available' do
@@ -21,13 +21,12 @@ describe DockingStation do
 
   it 'sees bike in DockingStation' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike).length).to be > 0
   end
 
-  it "doesn't accept a bike if full" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect { subject.dock(bike) }.to raise_error("Station already full")
+  it "doesn't accept a bike if full" do 
+    20.times { subject.dock(Bike.new) }
+    expect { subject.dock(Bike.new) }.to raise_error("Station already full")
   end
 
 end
